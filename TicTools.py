681c7560bf4bb2b21bab395fbe9286cc40f11393
Drahoxx <Drahoxx@users.noreply.github.com>
2021-09-27 23:50:26 +0000

    Update TicTools.py

--- TicTools.py
+++ TicTools.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 
 from sys import argv, stderr
-from os import listdir
+from os import listdir,mkdir
 from os.path import  isfile
 from importlib import import_module
 
+#CONSTANTES
+ERROR = "\x1b[31m\x1b[1m/!\\\x1b[0m ERROR :"
+PLG = "plugins"
+#Header
 print(r"""
 __/\\\\\\\\\\\\\\\______________________/\\\\\\\\\\\\\\\______________________________/\\\\\\_________________        
  _\///////\\\/////______________________\///////\\\/////______________________________\////\\\_________________       
@@ -17,24 +21,26 @@ __/\\\\\\\\\\\\\\\______________________/\\\\\\\\\\\\\\\________________________
         _______\///________\///_____\////////________\///___________\/////________\/////_____\/////////__\//////////__
 """)
 
-plugins = listdir('./plugins')
+if("plugins" not in listdir(".")):
+    mkdir("plugins")
+    print("You dont have any plugins installed... You should consider install some... You can use our super plugin manager with python3 ticpip.py -h")
+
+plugins = listdir("./"+PLG)
 
 def _import(plugin,argv):
     try:
         if not isfile(plugin) and plugin in plugins: plugin = plugin + '.' + plugin
-        import_module('plugins.' + plugin).main(argv)
+        import_module(PLG+'.' + plugin).main(argv)
     except:
-        print(f"\x1b[31m\x1b[1m/!\\\x1b[0m ERROR : {plugin} is not a plugin",file=stderr)
-args = {}
+        print(f"{ERROR} {plugin} is not a plugin",file=stderr)
 
 if len(argv) < 2 :
-    print("\x1b[31m\x1b[1m/!\\\x1b[0m ERROR : Missing Argument\n",file=stderr)
+    print(f"{ERROR} Missing Argument\n",file=stderr)
     exit()
+
+
 if '-p' in argv : _import(argv[argv.index('-p')+1],argv)
 else :
     print("Liste des plugins :\n- "+' \n- '.join(plugins))
 
-    
-
-
-
+   
